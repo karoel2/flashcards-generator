c2e83e0d1752b7fd3570015f7f776b7a2b38a5f6
karoel2 <karoel123@gmail.com>
2018-02-17 15:30:44 +0100

    generating word files with data provided from excel

--- fiszki.js
+++ fiszki.js
@@ -1,7 +1,42 @@
 var excel = require('node-xlsx').default;
 var fs = require('fs');
+var docxtemplater = require('docxtemplater')
+var JSZip = require('jszip');
 
-var data = fs.readFile('Book1.xlsx');
-
+//display excel file
 var excelData = excel.parse('Book1.xlsx');
-console.log(excelData[0].data);
+
+var template = {};
+
+excelData[0].data.forEach((el,i) => {
+	i++;
+	let x = 'ob'+i;
+	let y = 're'+i;
+	template[x] = el[0];
+	template[y] = el[1];
+});
+
+//create docx template
+var content = fs.readFileSync('templ.docx');
+var zip = new JSZip(content);
+var doc = new docxtemplater();
+doc.loadZip(zip);
+
+doc.setData(template);
+
+try {
+	doc.render();
+}
+catch (error) {
+    var e = {
+        message: error.message,
+        name: error.name,
+        stack: error.stack,
+        properties: error.properties,
+    }
+    console.log(JSON.stringify({error: e}));
+    throw error;
+}
+
+var buf = doc.getZip().generate({type: 'nodebuffer'});
+fs.writeFileSync('output2.docx', buf);
